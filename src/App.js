dc5db95311eb08cbb2c4257b4a6d0cf7ea07be41
Melksedeque <freelancer@melksedeque.com.br>
2025-01-18 22:23:00 -0300

    Limpeza de código não usado nativo do React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Banner from "./componentes/Banner/Banner.js";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-vindo ao React.JS
-          <br />
-          <small>Aqui é onde o filho chora e a mãe não vê!</small>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
