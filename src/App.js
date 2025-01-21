ae6480760a25e7878b4df04604001cc86a171546
Melksedeque <freelancer@melksedeque.com.br>
2025-01-21 19:15:11 -0300

    Criação de getter e setter para colaboradores; Criação de função para iterar a lista de colaboradores;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+  const colaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          colaboradorAdicionado(colaborador)
+        }
+      />
     </div>
   );
 }
