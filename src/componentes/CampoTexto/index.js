51bc30b1ba245f2f680c1d237c4082251abc7d9c
Melksedeque <freelancer@melksedeque.com.br>
2025-01-21 18:05:44 -0300

    Criação de função para capturar o que é digitado no campo de texto

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,13 +1,22 @@
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  let valor = "Melk";
+
+  const aoDigitado = (e) => {
+    valor = e.target.value;
+    console.log(valor);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}: </label>
       <input
         type={props.text}
+        value={valor}
         placeholder={props.placeholder}
         required={props.obrigatorio}
+        onChange={aoDigitado}
       />
     </div>
   );
