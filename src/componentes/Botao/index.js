0408a5c76a759dad6ef0668a44284357ce56c000
Melksedeque <freelancer@melksedeque.com.br>
2025-01-20 23:07:23 -0300

    Mudança de props para não chamar o texto, mas chamar as children

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,7 +1,7 @@
 import "./Botao.css";
 
 const Botao = (props) => {
-  return <button>{props.texto}</button>;
+  return <button className="botao">{props.children}</button>;
 };
 
 export default Botao;
