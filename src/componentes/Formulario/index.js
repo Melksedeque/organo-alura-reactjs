742003ef5606f54c68a0c3f4939b6f36919841d3
Melksedeque <freelancer@melksedeque.com.br>
2025-01-23 20:56:54 -0300

    Chamada do nome do time por props

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,16 +5,6 @@ import Botao from "../Botao";
 import { useState } from "react";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "DevOps",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
-
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
   const [imagem, setImagem] = useState("");
@@ -59,7 +49,7 @@ const Formulario = (props) => {
         />
         <ListaSuspensa
           label="Time"
-          itens={times}
+          itens={props.nomesTimes}
           obrigatorio={true}
           valor={time}
           aoAlterado={(valor) => setTime(valor)}
