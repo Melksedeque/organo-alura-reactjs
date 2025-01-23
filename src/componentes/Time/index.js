d4cc81ec61ce99362a104c8edd660f673b68a803
Melksedeque <freelancer@melksedeque.com.br>
2025-01-23 20:15:00 -0300

    Criação inicial hard-coded do componente Time

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,23 @@
+import "./Time.css";
+
+const Time = () => {
+  return (
+    <section>
+      <header>
+        <h3>Programação</h3>
+      </header>
+      <div className="colaboradores">
+        <div className="colaborador">
+          <figure className="imagem-colaborador">
+            <img src="" alt="Foto de Perfil da Juliana Amoasei" />
+            <figcaption hidden>Juliana Amoasei</figcaption>
+          </figure>
+          <h4>Juliana Amoasei</h4>
+          <p>Desenvolvedora de software e instrutora</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Time;
