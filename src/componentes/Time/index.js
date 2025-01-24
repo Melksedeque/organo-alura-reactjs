b4c36efb066eb9ce96c9f290c78924156e0810b3
Melksedeque <freelancer@melksedeque.com.br>
2025-01-23 22:07:02 -0300

    Condicional para só exibir as seções que possuem colaboradores

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,21 +5,23 @@ const Time = (props) => {
   const estiloTime = { backgroundColor: props.corSecundaria };
 
   return (
-    <section className="time" style={estiloTime}>
-      <header>
-        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      </header>
-      <div className="colaboradores">
-        {props.colaboradores.map((colaborador) => (
-          <Colaborador
-            cor={props.corPrimaria}
-            foto={colaborador.imagem}
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-          />
-        ))}
-      </div>
-    </section>
+    props.colaboradores.length > 0 && (
+      <section className="time" style={estiloTime}>
+        <header>
+          <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        </header>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              cor={props.corPrimaria}
+              foto={colaborador.imagem}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
