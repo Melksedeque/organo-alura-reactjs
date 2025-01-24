598b6dce04e681e34f12bf3704352e1a77233cff
Melksedeque <freelancer@melksedeque.com.br>
2025-01-23 21:02:08 -0300

    Chamada de componente Colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
@@ -9,14 +10,7 @@ const Time = (props) => {
         <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       </header>
       <div className="colaboradores">
-        <div className="colaborador">
-          <figure className="imagem-colaborador">
-            <img src="" alt="Foto de Perfil da Juliana Amoasei" />
-            <figcaption hidden>Juliana Amoasei</figcaption>
-          </figure>
-          <h4>Juliana Amoasei</h4>
-          <p>Desenvolvedora de software e instrutora</p>
-        </div>
+        <Colaborador />
       </div>
     </section>
   );
