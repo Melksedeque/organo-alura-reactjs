6043fb6b76cfd97c720a605b8058d0c73a2d317f
Melksedeque <freelancer@melksedeque.com.br>
2025-01-27 15:29:15 -0300

    Criação de onChange no input para enxergar a cor e manipular a cor de fundo dos times

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,12 +1,18 @@
 import Colaborador from "../Colaborador";
 import "./Time.css";
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, alterarCor, aoDeletar }) => {
   const estiloTime = { backgroundColor: time.corSecundaria };
-
+  console.log(time.corSecundaria);
   return (
     colaboradores.length > 0 && (
       <section className="time" style={estiloTime}>
+        <input
+          type="color"
+          className="input-cor"
+          value={time.corSecundaria}
+          onChange={(e) => alterarCor(e.target.value, time.nome)}
+        />
         <header>
           <h3>{time.nome}</h3>
           <span style={{ backgroundColor: time.corPrimaria }}></span>
