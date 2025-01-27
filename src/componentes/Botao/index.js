c4c7e3acf7adf3133644e20072b637da0dcffde9
Melksedeque <freelancer@melksedeque.com.br>
2025-01-27 18:09:30 -0300

    Pegando o texto do botão via props

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,7 +1,7 @@
 import "./Botao.css";
 
 const Botao = (props) => {
-  return <button className="botao">{props.children}</button>;
+  return <button className="botao">{props.texto}</button>;
 };
 
 export default Botao;
