997c09ac1a1cb083808a7871ef7bea2fb0cebbb2
Melksedeque <freelancer@melksedeque.com.br>
2025-01-27 17:01:35 -0300

    Uso do Hex to RGBA para alterar a cor de fundo do time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,28 +1,30 @@
+import hexToRgba from "hex-to-rgba";
 import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = ({ time, colaboradores, alterarCor, aoDeletar }) => {
-  const estiloTime = { backgroundColor: time.corSecundaria };
-  console.log(time.corSecundaria);
   return (
     colaboradores.length > 0 && (
-      <section className="time" style={estiloTime}>
+      <section
+        className="time"
+        style={{ backgroundColor: hexToRgba(time.cor, "0.25") }}
+      >
         <input
           type="color"
           className="input-cor"
-          value={time.corSecundaria}
+          value={time.cor}
           onChange={(e) => alterarCor(e.target.value, time.nome)}
         />
         <header>
           <h3>{time.nome}</h3>
-          <span style={{ backgroundColor: time.corPrimaria }}></span>
+          <span style={{ backgroundColor: time.cor }}></span>
         </header>
         <div className="colaboradores">
           {colaboradores.map((colaborador, index) => {
             return (
               <Colaborador
                 key={index}
-                cor={time.corPrimaria}
+                cor={time.cor}
                 colaborador={colaborador}
                 aoDeletar={aoDeletar}
               />
