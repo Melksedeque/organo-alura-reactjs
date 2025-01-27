8f5ca9f494632e612955a2c72c1efaeb744fb1c7
Melksedeque <freelancer@melksedeque.com.br>
2025-01-27 17:02:47 -0300

    Remoção da variavel cor secundaria; Troca de nome da corPrimaria para cor; Aplicação de cor unica para cada time;

--- src/App.js
+++ src/App.js
@@ -9,38 +9,31 @@ function App() {
   const [times, setTimes] = useState([
     {
       nome: "Programação",
-      corPrimaria: "#57C278",
-      corSecundaria: "#D9F7E9",
+      cor: "#57C278",
     },
     {
       nome: "Front-End",
-      corPrimaria: "#82CFFA",
-      corSecundaria: "#E8F8FF",
+      cor: "#82CFFA",
     },
     {
       nome: "Data Science",
-      corPrimaria: "#A6D157",
-      corSecundaria: "#F0F8E2",
+      cor: "#A6D157",
     },
     {
       nome: "DevOps",
-      corPrimaria: "#E06B69",
-      corSecundaria: "#FDE7E8",
+      cor: "#E06B69",
     },
     {
       nome: "UX e Design",
-      corPrimaria: "#DB6EBF",
-      corSecundaria: "#FAE9F5",
+      cor: "#DB6EBF",
     },
     {
       nome: "Mobile",
-      corPrimaria: "#FFBA05",
-      corSecundaria: "#FFF5D9",
+      cor: "#FFBA05",
     },
     {
       nome: "Inovação e Gestão",
-      corPrimaria: "#FF8A29",
-      corSecundaria: "#FFEEDF",
+      cor: "#FF8A29",
     },
   ]);
 
@@ -97,7 +90,7 @@ function App() {
     setTimes(
       times.map((time) => {
         if (time.nome === nome) {
-          time.corSecundaria = cor;
+          time.cor = cor;
         }
         return time;
       })
