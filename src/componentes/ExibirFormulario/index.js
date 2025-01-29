8b9185119567b8ca19df2d326af075b92b64c587
Melksedeque <freelancer@melksedeque.com.br>
2025-01-29 20:19:24 -0300

    Criação de componente para exibir ou ocultar formulario

---
+++ src/componentes/ExibirFormulario/index.js
@@ -0,0 +1,13 @@
+import "./ExibirFormulario.css";
+
+const ExibirFormulario = () => {
+  function exibirEsconderFormularios() {}
+
+  return (
+    <>
+      <button onClick={exibirEsconderFormularios()}></button>
+    </>
+  );
+};
+
+export default ExibirFormulario;
