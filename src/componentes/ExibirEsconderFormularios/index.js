8fc5fe2a35e1adea89eee9ef242755487f1e949a
Melksedeque <freelancer@melksedeque.com.br>
2025-01-29 21:22:54 -0300

    Criação de função para alternar estado dos formulários

--- src/componentes/ExibirEsconderFormularios/index.js
+++ src/componentes/ExibirEsconderFormularios/index.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./ExibirEsconderFormularios.css";
 
-const ExibirEsconderFormularios = () => {
-  function toggleFormularios() {}
+const ExibirEsconderFormularios = ({ exibir, aoAlternar }) => {
+  const [visualizacao, setVisualizacao] = useState("off");
+
+  function toggleFormularios(e) {
+    console.log(e);
+    setVisualizacao(visualizacao === "off" ? "on" : "off");
+  }
 
   return (
     <>
-      <button onClick={toggleFormularios()}></button>
+      <button className="exibir-esconder-formulario" onClick={aoAlternar}>
+        <img
+          src="/imagens/botao.png"
+          alt="Botão para exibir ou ocultar os Formulários"
+        />
+      </button>
     </>
   );
 };
