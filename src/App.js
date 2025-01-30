705418c6a37e786b15e83470a0524503c1467ea5
Melksedeque <freelancer@melksedeque.com.br>
2025-01-30 16:23:19 -0300

    Substituição das variáveis fixas com colaboradores e times por fetch ao arquivo db.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
@@ -8,96 +8,29 @@ import { v4 as uuidv4 } from "uuid";
 import ExibirEsconderFormularios from "./componentes/ExibirEsconderFormularios";
 
 function App() {
-  const [times, setTimes] = useState([
-    {
-      id: uuidv4(),
-      nome: "Programação",
-      cor: "#57C278",
-    },
-    {
-      id: uuidv4(),
-      nome: "Front-End",
-      cor: "#82CFFA",
-    },
-    {
-      id: uuidv4(),
-      nome: "Data Science",
-      cor: "#A6D157",
-    },
-    {
-      id: uuidv4(),
-      nome: "DevOps",
-      cor: "#E06B69",
-    },
-    {
-      id: uuidv4(),
-      nome: "UX e Design",
-      cor: "#DB6EBF",
-    },
-    {
-      id: uuidv4(),
-      nome: "Mobile",
-      cor: "#FFBA05",
-    },
-    {
-      id: uuidv4(),
-      nome: "Inovação e Gestão",
-      cor: "#FF8A29",
-    },
-  ]);
+  const urlTimes = "http://localhost:8080/times";
+  const urlPessoas = "http://localhost:8080/pessoas";
 
-  const inicial = [
-    {
-      id: uuidv4(),
-      nome: "Daniel Artine",
-      cargo: "Engenheiro de Software",
-      link: "https://github.com/danielartine",
-      time: times[0].nome,
-      favorito: false,
-    },
-    {
-      id: uuidv4(),
-      nome: "Guilherme Lima",
-      cargo: "Desenvolvedor Python e JavaScript",
-      link: "https://github.com/guilhermeonrails",
-      time: times[0].nome,
-      favorito: false,
-    },
-    {
-      id: uuidv4(),
-      nome: "Juliana Amoasei",
-      cargo: "Desenvolvedora de Softwares",
-      link: "https://github.com/julianaamoasei",
-      time: times[0].nome,
-      favorito: false,
-    },
-    {
-      id: uuidv4(),
-      nome: "Melksedeque Silva",
-      cargo: "Desenvolvedor Front-End",
-      link: "https://github.com/Melksedeque",
-      time: times[1].nome,
-      favorito: false,
-    },
-    {
-      id: uuidv4(),
-      nome: "Rodrigo Passos",
-      cargo: "Desenvolvedor Front-End",
-      link: "https://github.com/rpc1910",
-      time: times[1].nome,
-      favorito: false,
-    },
-    {
-      id: uuidv4(),
-      nome: "Amanda Domingues",
-      cargo: "Cientista de Dados",
-      link: "https://github.com/amandaamnd",
-      time: times[2].nome,
-      favorito: false,
-    },
-  ];
+  const [times, setTimes] = useState([]);
 
-  const [colaboradores, setColaboradores] = useState(inicial);
+  useEffect(() => {
+    fetch(urlTimes)
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setColaboradores(dados);
+      });
+  }, []);
+
+  const pessoasInicial = useEffect(() => {
+    fetch(urlPessoas)
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setColaboradores(dados);
+      });
+  }, []);
+
+  const [colaboradores, setColaboradores] = useState(pessoasInicial);
+  console.log(colaboradores);
   const [exibirFormulario, setExibirFormulario] = useState(false);
 
   const colaboradorAdicionado = (colaborador) => {
